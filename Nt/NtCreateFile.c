d2e0f15fd1fbbb0dfcd8f2a7bdbb02b12c70012d
Garhoogin <declan.c.moore@gmail.com>
2021-01-31 20:55:48 -0600

    Create NtCreateFile.c

---
+++ Nt/NtCreateFile.c
@@ -0,0 +1,3 @@
+NTSTATUS NtCreateFile(HANDLE FileHandle, ACCESS_MASK DesiredAccess, POBJECT_ATTRIBUTES ObjectAttributes, PIO_STATUS_BLOCK IoStatusBlock, PLARGE_INTEGER AllocationSize, ULONG FileAttributes, ULONG ShareAccess, ULONG CreateDisposition, ULONG CreateOptions, PVOID EaBuffer, ULONG EaLength){
+	return IopCreateFile(FileHandle, DesiredAccess, ObjectAttributes, IoStatusBlock, AllocationSize, FileAttributes, ShareAccess, CreateDisposition, CreateOptions, EaBuffer, EaLength, CreateFileTypeNone, NULL, 0, 0x20, NULL);
+}
